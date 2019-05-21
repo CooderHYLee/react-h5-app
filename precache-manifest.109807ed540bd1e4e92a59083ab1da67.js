1589ecc19ce91a784d917a2a083558fce604211c
lihongye <1121807592@qq.com>
2019-05-21 11:50:12 +0800

    Updates

---
+++ precache-manifest.109807ed540bd1e4e92a59083ab1da67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0049caade378b9163bea55e7757e26b2",
+    "url": "/react-h5-app/index.html"
+  },
+  {
+    "revision": "522df6ee3009347df601",
+    "url": "/react-h5-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-h5-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "522df6ee3009347df601",
+    "url": "/react-h5-app/static/js/main.1a95ac88.chunk.js"
+  },
+  {
+    "revision": "7a02b57c7a8de01dbc2b",
+    "url": "/react-h5-app/static/js/runtime~main.52959f85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-h5-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
